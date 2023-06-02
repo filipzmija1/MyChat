101cd32aa25adaad28c9aeeafd972052b234a312
filipzmija1 <zmijafilip@gmail.com>
2023-06-02 18:37:22 +0200

    room and messages models

--- mychat/mychat/settings.py
+++ mychat/mychat/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'viperchat.apps.ViperchatConfig'
 ]
 
 MIDDLEWARE = [
--- mychat/viperchat/models.py
+++ mychat/viperchat/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Room(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    users = models.ManyToManyField(User)
+    is_private = models.BooleanField(default=False)
+
+
+class Message(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    
