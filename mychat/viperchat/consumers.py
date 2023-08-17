d86deb6233fd4af27aecad6685b5a01753d7deb8
filipzmija1 <zmijafilip@gmail.com>
2023-08-17 16:34:55 +0200

    consumers

--- mychat/viperchat/consumers.py
+++ mychat/viperchat/consumers.py
@@ -6,9 +6,10 @@ from channels.db import database_sync_to_async
 from .models import Message, Room, Server
 
 
-class ChatConsumer(AsyncWebsocketConsumer):
+class RoomChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.room_id = self.scope["url_route"]["kwargs"]["server_id"]["pk"]
+        self.server_id = self.scope["url_route"]["kwargs"]["server_id"]
+        self.room_id = self.scope["url_route"]["kwargs"]["pk"]
         self.room_group_name = f"chat_{self.room_id}"
 
         # Join room group
@@ -24,27 +25,38 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
+        username = self.scope["user"].username
         room = await get_room(self.room_id)
         # Send message to room group
         await self.channel_layer.group_send(
-            self.room_group_name, {"type": "chat.message", "message": message}
+            self.room_group_name, {"type": "chat.message", "message": message, "username": username}
         )
-        
-        await save_message(self.scope['user'], message, room)
+        # Save messages to database
+        await save_room_message(self.scope['user'], message, room)
 
     # Receive message from room group
     async def chat_message(self, event):
         message = event["message"]
-
+        username = event['username']
         # Send message to WebSocket
-        await self.send(text_data=json.dumps({"message": message}))
+        await self.send(text_data=json.dumps({"message":f'{username}: {message}'}))
 
 
 @database_sync_to_async
-def save_message(author, content, room):
+def save_room_message(author, content, room):
     return Message.objects.create(author=author, content=content, room=room)
 
 
 @database_sync_to_async
 def get_room(room_id):
     return Room.objects.get(id=room_id)
+
+
+@database_sync_to_async
+def save_private_message(author, content, receiver):
+    return Message.objects.create(author=author, content=content, message_receiver=receiver)
+
+
+@database_sync_to_async
+def get_server(server_id):
+    return Server.objects.get(id=server_id)
--- mychat/viperchat/routing.py
+++ mychat/viperchat/routing.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import consumers
 
 websocket_urlpatterns = [
-    path('ws/server/<uuid:server_id>/rooms/<uuid:pk>/', consumers.ChatConsumer.as_asgi()),
+    path('ws/server/<uuid:server_id>/rooms/<uuid:pk>', consumers.RoomChatConsumer.as_asgi()),
 ]
